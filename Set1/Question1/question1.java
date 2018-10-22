217b9227b0b7168700d77204e662c53928b6e7f4
Siddharth Syal <40547786+siddharthsyal@users.noreply.github.com>
2018-10-22 14:46:53 -0400

    Update question1.java

--- Set1/Question1/question1.java
+++ Set1/Question1/question1.java
@@ -1,3 +1,11 @@
+//Convert hex to base64
+//The string:
+//49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d
+//
+//Should produce:
+//SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t
+//Note : Dont use inbuilt libraries. Build everything from scratch.
+
 import java.util.Scanner;
 import java.math.*;
 public class question1 {
